2c3fb3102b6bfcd313ece394f289b5f2afc1f7d6
pachigallashalini <shaliniswar@gmail.com>
2019-10-14 13:30:18 +0100

    Unit tests

---
+++ EasyJetTestTests/UserRespositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyJetTestTests
+{
+    [TestClass]
+    public class UserRespositoryTests
+    {
+        private UserRepository _userRepository;
+        [TestInitialize]
+        public void Initialise()
+        {
+            _userRepository = new UserRepository();
+        }
+    }
+}
